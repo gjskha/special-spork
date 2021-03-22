465e5405a9c4e1c6b562a4bb49b84f6079990f8b
gjskha <gjskha@gmail.com>
2021-03-21 21:44:09 -0700

    making interactive

--- sorting/illustrate.sh
+++ sorting/illustrate.sh
@@ -1,21 +1,32 @@
-echo "Bubble Sort"
+#!/bin/bash
 
-declare -a the_array=(8 4 2 1 7 11 )
+BLACK="\033[1;30m"
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+LIGHT_BLUE="\033[1;34m"
+VIOLET="\033[1;35m"
+TURQUIOSE="\033[1;36m"
+WHITE="\033[1;37m"
+NO_COLOUR="\033[0m"
 
-echo -n "Starting with the array:"
-for elem in "${the_array[@]}" ; do
-     echo -n "$elem "
-done
-echo 
+ALGORITHMS="Bubble Insertion Quit"
+
+declare -a the_array=(8 4 2 1 7 11)
+a_count=0
+
+#for elem in "${the_array[@]}" ; do
+#     echo -n "$elem "
+#done
 
 function bubblesort() {
+
+    echo "Bubble Sort"
 
     # weird hacky stuff to pass array in bash
     name=$1[@]
     the_array=("${!name}")
     n=$2
-
-    #echo "Iteration $n"
 
     # subshell only
     if [ $n -eq 1 ]; then
@@ -35,7 +46,7 @@ function bubblesort() {
         echo -n "Round $n, iteration $this_iteration. Is ${the_array[$this_iteration]} less than ${the_array[$next_cell_up]}?"
 
         if [[ ${the_array[$this_iteration]} -lt ${the_array[$next_cell_up]} ]]; then
-            echo " Yes. Switching places"
+            echo -e "$RED Yes. Switching places $NO_COLOUR "
             tmp=${the_array[$this_iteration]}
             the_array[$this_iteration]=${the_array[$next_cell_up]} 
             the_array[$next_cell_up]=$tmp 
@@ -59,20 +70,9 @@ function bubblesort() {
     bubblesort the_array $n
 }
 
-bubblesort the_array 6
-
-echo
-echo "Insertion Sort"
-
-declare -a the_array=(11 42 2 55 6 13 1 22)
-
-echo -n "Starting with the array:"
-for elem in "${the_array[@]}" ; do
-     echo -n "$elem "
-done
-echo 
-
 function insertionsort() {
+
+    echo "Insertion Sort"
 
     # weird hacky stuff to pass array in bash
     name=$1[@]
@@ -96,7 +96,7 @@ function insertionsort() {
 	while [ $next_cell_down -ge 0 ] && [ ${the_array[$next_cell_down]} -lt $this_iterations_value ] ; do
 	#while [ $next_cell_down -ge 0 ] && [ ${the_array[$next_cell_down]} -lt ${the_array[$this_iteration]} ] ; do
       
-            echo "at outer iteration $this_iteration and inner iteration $next_cell_down : $this_iterations_value is greater than ${the_array[$next_cell_down]}"
+            echo -e "at outer iteration $this_iteration and inner iteration $next_cell_down : $RED $this_iterations_value is greater than ${the_array[$next_cell_down]} $NO_COLOUR "
 
             k=$(($next_cell_down + 1))
 	    tmp=${the_array[$k]}
@@ -118,15 +118,51 @@ function insertionsort() {
 
 }
 
-insertionsort the_array 8
+#declare -a the_array=(10 9 8 7 6 5 4 3 2 1 11)
+#insertionsort the_array 11
 
-declare -a the_array=(10 9 8 7 6 5 4 3 2 1 11)
+function accept_nums() {
 
-echo -n "Starting with the array:"
-for elem in "${the_array[@]}" ; do
-     echo -n "$elem "
-done
-echo 
+    echo "Enter a number to sort, or Q for quit:"
 
-insertionsort the_array 11
+    while [[ true ]]; do
+        read REPLY
+        if [[ $REPLY ==	[Qq] ]]; then
+            break
+        else
+            # grab input
+            a_count=$(( a_count + 1 ))
+        fi
+    done
 
+    #echo a_count is $a_count
+}
+
+function main_loop() {
+
+    echo "A program for illustrating sort algorithms."
+
+    accept_nums
+
+    echo "Select an algorithm:"
+
+    until [[ "$choice" == [Qq] ]]; do
+        select choice in $ALGORITHMS; do
+            case $choice in
+                Bubble)
+                    # temporarily hard coded
+                    bubblesort the_array 6
+                ;;
+                Insertion)
+                    insertionsort the_array 6
+                ;;
+                *)
+                    return
+                ;;
+            esac
+            break
+        done
+    done
+}
+
+main_loop
